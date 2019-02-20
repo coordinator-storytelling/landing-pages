eca89bc2c8c92b6e7a33f3aff98bed301b527764
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-21 00:17:47 +0800

    Update script

--- js/generateDoc.js
+++ js/generateDoc.js
@@ -1,18 +1,18 @@
-function readTextFile(file) {
-  var rawFile = new XMLHttpRequest();
-  rawFile.open("GET", file, false);
-  rawFile.onreadystatechange = function () {
-    if (rawFile.readyState === 4) {
-      if (rawFile.status === 200 || rawFile.status == 0) {
-        var allText = rawFile.responseText;
-        alert(allText);
-      }
-    }
-  }
-  rawFile.send(null);
-}
-let myFile = readTextFile("file:///docs/MEDIA.md")
-console.log(myFile)
+// function readTextFile(file) {
+//   var rawFile = new XMLHttpRequest();
+//   rawFile.open("GET", file, false);
+//   rawFile.onreadystatechange = function () {
+//     if (rawFile.readyState === 4) {
+//       if (rawFile.status === 200 || rawFile.status == 0) {
+//         var allText = rawFile.responseText;
+//         alert(allText);
+//       }
+//     }
+//   }
+//   rawFile.send(null);
+// }
+// let myFile = readTextFile("file:///docs/MEDIA.md")
+// console.log(myFile)
 
 // const input = document.querySelector('input[type="file"]')
 // input.addEventListener('change', function (e) {
