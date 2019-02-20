0203130d844cb2c4f4c15ce6380484e7095fbec0
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-21 00:44:57 +0800

    Add production version media credits page

---
+++ js/generateMediaDoc.js
@@ -0,0 +1,11 @@
+let url = "https://bountifulapp.netlify.com/docs/MEDIA.md"
+
+async function getData (url) { 
+  let downloadData = await fetch(url) 
+  let data = await downloadData.text()
+  document.getElementById('content').innerHTML = marked(data)
+  console.log(`data: `, data)
+}
+
+getData(url)
+
