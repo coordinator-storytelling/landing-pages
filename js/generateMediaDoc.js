2deda16590689a1d82f1fc0b607f3a7c439418fd
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-21 17:16:33 +0800

    Define variables of table elements

--- js/generateMediaDoc.js
+++ js/generateMediaDoc.js
@@ -1,12 +1,21 @@
+
 let url = "https://bountifulapp.netlify.com/docs/MEDIA.md"
 
-async function getData (url) { 
-  let downloadData = await fetch(url) 
+async function getData(url) {
+  let downloadData = await fetch(url)
   let data = await downloadData.text()
   document.getElementById('content').innerHTML = marked(data)
   console.log(`data: `, data)
 }
 
 window.onload = getData(url)
+
+//Create table
+const mainContent = document.getElementById('content')
+const tableHeader = document.querySelector('thead')
+console.log(`tableHeader: ${tableHeader}`)
+const tableBody = document.querySelector('tbody')
+console.log(`tableBody: ${tableBody}`)
+const tableWrapper = document.createElement('div')
 
 
